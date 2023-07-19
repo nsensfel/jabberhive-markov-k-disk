e54120f953e00f9abd175fb715aa36574ac70961
Nathanael Sensfelder <spamshield0@noot-noot.org>
2023-07-19 12:25:23 +0200

    Adds run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-CURRENT_DIRECTORY=$(cd `dirname $0` && pwd)
-
-if [[ "$#" != "1" ]];
-then
-   echo "Expected single argument: socket file."
-   exit
-fi
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 while true
 do
    rm -f $1
-   # Run with listenin socket on $1 and Markov rank 3.
-   $CURRENT_DIRECTORY/jabberhive-markov-k-ram $1 3
+   $SCRIPT_DIR/jh-markov-k-disk $1 $2 $3
+   sleep 60
 done
