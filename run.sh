acb090e728b995490258f6acf8761cc97aaed6dc
NATHANAEL SENSFELDER <nathanael.sensfelder@nxp.com>
2023-07-26 13:03:26 +0200

    Shouldn't have commited with valgrind in there.

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while true
 do
    rm -f $1
-   valgrind --leak-check=full $SCRIPT_DIR/jh-markov-k-disk $1 $2 $3
+   $SCRIPT_DIR/jh-markov-k-disk $1 $2 $3
    sleep 60
 done
